da04cb22d7354e912bb6eed16ee35dde3610286d
saxenaraman2103-arch <saxena.raman2103@gmail.com>
2026-08-14 21:58:42 +0530

    update terraform version

--- environments/prod/variables.tf
+++ environments/prod/variables.tf
@@ -1,11 +1,24 @@
 variable "resource_group" {
-  type = string
+  type = map(object({
+    name     = string
+    location = string
+  }))
 }
 variable "virtual_network" {
-
+  type = map(object({
+    name                = string
+    resource_group_name = string
+    location            = string
+    address_space       = list(string)
+  }))
 }
 variable "subnets" {
-  type = string
+  type = map(object({
+    name                 = string
+    resource_group_name  = string
+    virtual_network_name = string
+    address_prefixes     = list(string)
+  }))
 }
 
 
--- environments/prod/version.tf
+++ environments/prod/version.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 1.6.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "5.0.1"
+      version = "~>5.0.1"
     }
   }
 }
