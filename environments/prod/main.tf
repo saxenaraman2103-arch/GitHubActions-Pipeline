59d93c093bd8d0e577ca253f11a9cd9bbb122ac5
saxenaraman2103-arch <saxena.raman2103@gmail.com>
2026-08-16 00:29:16 +0530

    update main tf

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -6,23 +6,23 @@ module "azurerm_resource_group" {
 module "azurerm_virtual_network" {
   source     = "../../modules/virtual-network"
   config     = var.virtual_network
-  depends_on = [module.azurerm_resource_group]
+  depends_on = [module.resource_group]
 }
 
 module "azurerm_subnet" {
   source     = "../../modules/subnet"
   config     = var.subnets
-  depends_on = [module.azurerm_virtual_network]
+  depends_on = [module.virtual_network]
 }
 
 module "network_security_group" {
   source     = "../../modules/network-security-group"
   config     = var.network_security_group
-  depends_on = [module.azurerm_resource_group]
+  depends_on = [module.resource_group]
 }
 
-module "subnet_network_association_group" {
+module "subnet_network_association_group_association" {
   source     = "../../modules/subnet-network-association-group"
   config     = var.subnet_network_association_group
-  depends_on = [module.network_security_group, module.azurerm_subnet]
+  depends_on = [module.network_security_group, module.subnet]
 }
